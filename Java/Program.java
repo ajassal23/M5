8babdf9c2d53334ab87016c1d5b3e1f19aeccb7c
GS <GS@I02-14>
2024-01-25 20:39:47 +0100

    prueba

--- Java/Program.java
+++ Java/Program.java
@@ -1,7 +1,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
+ * Copyright 2020 Joan Sculi <jseculi@escoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
