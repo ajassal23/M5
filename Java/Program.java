fddb3240da657c5aa9a789678c3bbb7d94c48676
GS <GS@I02-13>
2024-02-02 18:23:20 +0100

    fbfdfsdfvsfd

--- Java/Program.java
+++ Java/Program.java
@@ -12,19 +12,7 @@ public class Program {
 	public static double perimeter(double radius) {
 
 		return (2 * Math.PI) * radius;
-	}
-	public static void main(String[] args) {
-
-		Scanner in = new Scanner(System.in);
-
-		System.out.println("Enter the radius: ");
-		double radi = in.nextDouble();
-
-		in.close();
-
-		System.out.println("\nThe area is: " + area(radi));
-
-		System.out.println("\nThe perimeter is: " + perimeter(radi));
+	} is: " + perimeter(radi));
 
 	}
 }
